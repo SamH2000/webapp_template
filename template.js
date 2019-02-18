0e0ff084917e36ab9d49e2e7b06d66f70c39edf0
sam <sam.heiden@ictmbo.nl>
2019-02-18 12:42:16 +0100

    init

--- template.js
+++ template.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function(event) {
     alert("DOM is geladen");
-=======
-document.addEventListener("DOMContentLoaded", function(event) {
-    alert("DOM is geladen");
->>>>>>> 3b210fbab4f2c2e43b3ea938f60bbd567fd57b36
 });
